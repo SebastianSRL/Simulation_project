22ed20887d54f89722517964619c87c5a572ef61
Sebastian <sebastian.rivera.leon2000@gmail.com>
2022-03-01 16:43:15 -0500

    V1.1 - Jail and Go cell added

--- entities.py
+++ entities.py
@@ -5,9 +5,44 @@ class Player:
     def __init__(self, name, money):
         self.name = name
         self.money = money
+        self.earned = money
+        self.spend = 0
         self.position = 0
-        self.jail = False
-        self.bankrupt = False
+        self.turn_counter = 0
+        self.go_counter = 0
+        self.__jail = False
+        self.__bankrupt = False
+
+    def bankruptcy(self):
+        """
+        Return the player's state
+        :return: bankrupt
+        """
+        return self.__bankrupt
+
+    def jail(self):
+        """
+        Set the jail property to True
+        """
+        self.__jail = True
+
+    def pay(self, amount):
+        """
+        Pays and register the amount on the total spent by the player
+        :param amount: Amount to pay
+        :return: None
+        """
+        self.money -= amount
+        self.spend -= amount
+
+    def earn(self, amount):
+        """
+        Saves and registers the amount on the total earned by the player
+        :param amount: Amount to save
+        :return: None
+        """
+        self.money += amount
+        self.earned += amount
 
     def turn(self, move, board, players):
         """
@@ -17,28 +52,62 @@ class Player:
         :param board: Player's board
         :param players: List of players
         """
-        board_size = len(board)
+        self.turn_counter += 1  # Cuenta el turno jugado o perdido
 
-        if self.position + move >= board_size:  # Altera la posicion del jugador teniendo en cuenta si ya va a dar una vuelta o no
-            move = move - (board_size - self.position)
-            self.position = move
-        else:
-            self.position += move
+        if not self.__jail:  # Si no cayó en la carcel previamente juega el turno
 
-        standing = board[self.position - 1]  # Propiedad sobre la cual ahora se encuentra situado el jugador
+            board_size = len(board)
 
-        if standing.owner is None:  # Si la propiedad no tiene dueño
-            if standing.value <= self.money:  # Si tiene dinero para comprarla
-                self.money -= standing.value
-                standing.set_owner(self.name)
-        else:  # Si tiene dueño
-            if standing.fee <= self.money:
-                for player in players:
-                    if standing.owner == player.name and player.name != self.name:  # Busca el dueño y que no sea uno mismo
-                        self.money -= standing.fee  # Paga
-                        player.money += standing.fee
-            else:  # Si no puede pagar: bancarrota
-                self.bankrupt = True
+            # ================ #
+            #     Movement     #
+            # ================ #
+
+            if self.position + move >= board_size:  # Altera la posicion del jugador teniendo en cuenta si ya va a dar una vuelta o no
+                move = move - (board_size - self.position)
+                self.position = move
+                self.go_counter += 1  # Contador de vuetas al tablero
+                # self.earn(5)  # Da x cantidad en cada vuelta al tablero
+            else:
+                self.position += move
+
+            standing = board[self.position - 1]  # Propiedad sobre la cual ahora se encuentra situado el jugador
+
+            # ================ #
+            #    Behaviour     #
+            # ================ #
+
+            if standing.get_name() == 'GO':  # Si pasa por la salida no hace nada a simple vista
+                pass  # El dinero por darle la vuelta al tablero se añade en otra parte
+            elif standing.get_name() == 'Jail':
+                self.jail()  # Si cae en la carce lo hace perder un turno
+            else:
+
+                if standing.get_owner() is None:  # Si la propiedad no tiene dueño
+                    if standing.get_value() <= self.money:  # Si tiene dinero para comprarla
+                        standing.set_owner(self.name)
+                        self.pay(standing.get_value())  # Registro de dinero gastado
+
+                else:  # Si tiene dueño
+                    if standing.get_fee() <= self.money:
+
+                        # Busca el dueño y que no sea el mismo jugador
+                        for player in players:
+                            if standing.get_owner() == player.name and player.name != self.name:
+                                self.pay(standing.get_fee())  # Paga
+                                player.earn(standing.get_fee())  # Registra las ganancias al jugador correspondiente
+
+                    else:
+
+                        # Habilita todas las propiedades del jugador que entro en bancarrota para ser compradas
+                        for prop in board:
+                            if prop.get_owner() == self.name and prop.get_name():
+                                prop.set_owner(None)
+
+                        # Si no puede pagar: bancarrota
+                        self.__bankrupt = True
+
+        else:  # Si cayó en la carcel previamente solo se cambia el atributo para jugar el siguiente turno
+            self.__jail = False
 
 
 # No usado en la versión actual
@@ -64,16 +133,49 @@ class Bank:
         self.money += cash
 
 
-class Property:
+# Super clase celda
+class Cell:
+    def __init__(self, name):
+        self.__name = name
+
+    def get_name(self):
+        return self.__name
+
+    def get_owner(self):
+        pass
+
+
+# Carcel
+class Jail(Cell):
+    def __init__(self):
+        self.__name = 'Jail'
+        super().__init__(self.__name)
+
+    # Hace perder un turno a un jugador
+    @staticmethod
+    def punish(player):
+        player.set_jail()
+
+
+class Property(Cell):
     def __init__(self, name, value, fee):
-        self.value = value
-        self.fee = fee
-        self.position = 0
-        self.owner = None
+        self.__value = value
+        self.__fee = fee
+        self.__owner = None
+        super().__init__(name)
+
+    def get_fee(self):
+        return self.__fee
+
+    def get_value(self):
+        return self.__value
+
+    def get_owner(self):
+        return self.__owner
 
     def set_owner(self, owner):
         """
         Set the owner of the property.
         :param owner:
         """
-        self.owner = owner
+        self.__owner = owner
--- main.py
+++ main.py
@@ -32,11 +32,13 @@ def main():
 
     # Inicializa las propiedades
     board = [
+        Cell('GO'),
         Property(name='Mediterranean Avenue', value=40, fee=2),
         Property(name='Baltic Avenue', value=60, fee=4),
         Property(name='Oriental Avenue', value=100, fee=6),
         Property(name='Vermont Avenue', value=100, fee=6),
         Property(name='Connecticut Avenue', value=100, fee=8),
+        Jail(),
         Property(name='St. Charles place', value=120, fee=10),
         Property(name='States Avenue', value=140, fee=10),
         Property(name='Virginia Avenue', value=160, fee=12),
@@ -49,6 +51,7 @@ def main():
         Property(name='Atlantic Avenue', value=260, fee=22),
         Property(name='Ventnor Avenue', value=260, fee=22),
         Property(name='Marvin Gardens', value=280, fee=24),
+        Jail(),
         Property(name='Pacific Avenue', value=300, fee=26),
         Property(name='North Carolina Avenue', value=300, fee=26),
         Property(name='Pennsilvania Avenue', value=320, fee=28),
@@ -66,21 +69,31 @@ def main():
 
     # Ordena el los jugadores e imprime el orden
     players = sort_players(players)
-    print(f"Orden de lanzamiento de dados: {[player.name for player in players]}")
+    print(f"Orden de lanzamiento de dados: {[player.name for player in players]}\n")
 
     while len(players) >= 2:  # Mientras haya al menos dos jugadores en juego
 
         for i, player in enumerate(players):  # Hace que todos los jugadores activos jueguen
-            movement = throw_dice() + throw_dice()  # Lanzamiendo de los dados
-            player.turn(movement, board, players)  # Juega el turno de un jugador
+            if len(players) >= 2:  # Antes de jugar el siguiente turno verfica si hay al menos dos jugadores activos
+                if player.bankruptcy():  # Si un jugador entra en bancarrota los saca de los jugadores activos y lo mete en los de bancarrota
+                    bankrupt_players.append(player)
+                    players.pop(i)
+                else:
+                    movement = throw_dice() + throw_dice()  # Lanzamiendo de los dados
+                    player.turn(movement, board, players)  # Juega el turno de un jugador
+            else:
+                break
 
-            if player.bankrupt:  # Si un jugador entra en bancarrota los saca de los jugadores activos y lo mete en los de bancarrota
-                bankrupt_players.append(player)
-                players.pop(i)
+    # Imprime un resumen del juego
+    print(f" !!!! El ganador fue: {players[0].name} con ${players[0].money} !!!! \n")
+    print(f"""Resumen del juego:\n
+    {players[0].name} gastó: {players[0].spend * -1} y ganó: {players[0].earned}  jugando {players[0].turn_counter} turnos y {players[0].go_counter} vueltas al tablero
+    {bankrupt_players[2].name} gastó: {bankrupt_players[2].spend * -1} y ganó: {bankrupt_players[2].earned} jugando {bankrupt_players[2].turn_counter} turnos y {bankrupt_players[2].go_counter} vueltas al tablero
+    {bankrupt_players[1].name} gastó: {bankrupt_players[1].spend * -1} y ganó: {bankrupt_players[1].earned} jugando {bankrupt_players[1].turn_counter} turnos y {bankrupt_players[1].go_counter} vueltas al tablero
+    {bankrupt_players[0].name} gastó: {bankrupt_players[0].spend * -1} y ganó: {bankrupt_players[0].earned} jugando {bankrupt_players[0].turn_counter} turnos y {bankrupt_players[0].go_counter} vueltas al tablero
 
-    print(f"El ganador fue: {players[0].name} con ${players[0].money}")
+    """)
     return -1
-
 
 
 if __name__ == '__main__':
