0ac75ed9ad99e248c458087f090010fb6cc196e1
Sebastian <sebastian.rivera.leon2000@gmail.com>
2022-03-09 13:52:42 -0500

    VF

--- entities.py
+++ entities.py
@@ -10,6 +10,8 @@ class Player:
         self.position = 0
         self.turn_counter = 0
         self.go_counter = 0
+        self.owned_props = []
+        self.debt = 0
         self.__jail = False
         self.__bankrupt = False
 
@@ -20,20 +22,76 @@ class Player:
         """
         return self.__bankrupt
 
+    # Simula el lanzamiento de un dado
+    def throw_dice(self):
+        """
+        Simulates a dice
+        :return: the result of throwing a dice
+        """
+        return np.random.randint(1, 7)
+
     def jail(self):
         """
         Set the jail property to True
         """
         self.__jail = True
 
-    def pay(self, amount):
+    def compare(self, amount):
+        return amount <= self.money
+
+    def pay(self, amount, board, debt=True):
         """
         Pays and register the amount on the total spent by the player
+        :param debt:
+        :param board:
         :param amount: Amount to pay
         :return: None
         """
-        self.money -= amount
-        self.spend -= amount
+        # Si el pago no es voluntario como comprar una propiedad
+        if debt:
+            # Compara si tiene sufieciente dinero para pagar
+            if self.compare(amount):
+                # Si tiene, paga
+                self.money -= amount
+                self.spend -= amount
+            else:
+                # Si no tiene mira que propiedades tienen menor valor
+                save = np.zeros((len(self.owned_props),))
+                for i, prop in enumerate(self.owned_props):
+                    # Guarda el valor de cada propiedad en un arreglo
+                    save[i] = prop.get_value()
+
+                # Se mira si vendiendo todas le alcanza para pagar
+                if self.compare(amount - np.sum(save)):
+                    # Si le alcanza busca las n propiedades de menor valor suficientes
+                    # para pagar la deuda
+                    payment = 0
+                    order = np.argsort(save).ravel()
+                    for j, ord in enumerate(order):
+                        payment += save[ord]
+                        if self.compare(amount - payment):
+                            break
+
+                    # Obtiene el valor de cada una de las propiedades vendidas
+                    self.earn(payment)
+                    # Se paga la deuda
+                    self.money -= amount
+                    self.spend -= amount
+
+                    # Se desvincula cada una de las propiedades vendidas del jugador actual
+                    for prop in self.owned_props:
+                        if prop.get_value() in save[order[:j + 1]]:
+                            prop.set_owner(None)
+                            self.owned_props.remove(prop)
+
+                else:
+                    # Si no puede pagar: bancarrota
+                    self.__bankrupt = True
+                    self.debt = amount
+        else:
+            # Si es una compra voluntaria como una propiedad
+            self.money -= amount
+            self.spend -= amount
 
     def earn(self, amount):
         """
@@ -45,7 +103,9 @@ class Player:
         self.earned += amount
 
     def movement(self, move, board_size):
-        if self.position + move >= board_size:  # Altera la posicion del jugador teniendo en cuenta si ya va a dar una vuelta o no
+
+        # Altera la posicion del jugador teniendo en cuenta si ya va a dar una vuelta o no
+        if self.position + move >= board_size:
             move = move - (board_size - self.position)
             self.position = move
             self.go_counter += 1  # Contador de vuetas al tablero
@@ -53,7 +113,7 @@ class Player:
         else:
             self.position += move
 
-    def turn(self, move, board, players):
+    def turn(self, move, board, players, magic_card=False):
         """
         Initialize a player given the name and the initial money.
 
@@ -61,7 +121,8 @@ class Player:
         :param board: Player's board
         :param players: List of players
         """
-        self.turn_counter += 1  # Cuenta el turno jugado o perdido
+        if not magic_card:
+            self.turn_counter += 1  # Cuenta el turno jugado o perdido
 
         if not self.__jail:  # Si no cayó en la carcel previamente juega el turno
 
@@ -77,65 +138,49 @@ class Player:
             # ================ #
             #    Behaviour     #
             # ================ #
+
             # Si pasa por la salida o parada libre no hace nada (El dinero se añade en la función turn)
+
             if standing.get_name() == 'GO' or standing.get_name() == 'Free Parking':
-                pass
+                pass  # Casilla libre
+            elif standing.get_name() == 'Card':
+                # No toma el valor del limite superior para generar los números
+                action = np.random.randint(1, 4)
+                # Se selecciona una valor a pagar o recibir de los disponibles
+                amount = standing.get_amount()[np.random.randint(0, 6)]
+                if action == 1:
+                    self.pay(amount, board)  # Pago por la tarjeta
+                elif action == 2:
+                    self.earn(amount)  # Recibe dinero por la tarjeta
+                else:
+                    # Movimiento por tarjeta
+                    move = np.random.randint(1, 4)  # Se elige de manera random cuantas casillas se va a mover [1,3]
+                    self.turn(move, board, players, magic_card=True)
+
             elif standing.get_name() == 'Jail':
                 self.jail()  # Si cae en la carce lo hace perder un turno
             else:
 
                 if standing.get_owner() is None:  # Si la propiedad no tiene dueño
-                    if standing.get_value() <= self.money:  # Si tiene dinero para comprarla
-                        standing.set_owner(self.name)
-                        self.pay(standing.get_value())  # Registro de dinero gastado
+                    if self.compare(standing.get_value()):  # Si tiene dinero para comprarla
+                        if np.random.rand(1) > 0.8:  # Decisión de compra
+                            standing.set_owner(self.name)
+                            self.pay(standing.get_value(), None, debt=False)  # Registro de dinero gastado
+                            self.owned_props.append(standing)
 
                 else:  # Si tiene dueño
-                    if standing.get_fee() <= self.money:
+                    # Busca el dueño y que no sea el mismo jugador
+                    for player in players:
+                        if standing.get_owner() == player.name and player.name != self.name:
+                            self.pay(standing.get_fee(), board)  # Paga
+                            player.earn(standing.get_fee())  # Registra las ganancias al jugador correspondiente
 
-                        # Busca el dueño y que no sea el mismo jugador
-                        for player in players:
-                            if standing.get_owner() == player.name and player.name != self.name:
-                                self.pay(standing.get_fee())  # Paga
-                                player.earn(standing.get_fee())  # Registra las ganancias al jugador correspondiente
-
-                    else:
-
-                        # Habilita todas las propiedades del jugador que entro en bancarrota para ser compradas
-                        for prop in board:
-                            if prop.get_owner() == self.name and prop.get_name():
-                                prop.set_owner(None)
-
-                        # Si no puede pagar: bancarrota
-                        self.__bankrupt = True
-
-        else:  # Si cayó en la carcel previamente solo se cambia el atributo para jugar el siguiente turno
+        # Si cayó en la carcel previamente solo se cambia el atributo para jugar el siguiente turno
+        else:
             self.__jail = False
 
 
-# No usado en la versión actual
-class Bank:
-    def __init__(self, money=np.inf):
-        self.money = money
-
-    def give_money(self, cash):
-
-        if self.money > 0:
-            if self.money > cash:
-                loan = cash
-                self.money -= cash
-            else:
-                loan = self.money
-                self.money = 0
-        else:
-            loan = 0
-
-        return loan
-
-    def get_money(self, cash):
-        self.money += cash
-
-
-# Super clase celda
+# Super clase celda, celdas o casillas donde no se realiza una acción
 class Cell:
     def __init__(self, name):
         self.__name = name
@@ -159,11 +204,21 @@ class Jail(Cell):
         player.set_jail()
 
 
+class Card(Cell):
+    def __init__(self):
+        self.__name = 'Card'
+        self.__amount = [15, 20, 50, 100, 150, 200]
+        super().__init__(self.__name)
+
+    def get_amount(self):
+        return self.__amount
+
+
 class Property(Cell):
     def __init__(self, name, value, fee):
-        self.__value = value
-        self.__fee = fee
-        self.__owner = None
+        self.__value = value  # Valor de la propiedad
+        self.__fee = fee  # Valor de la renta
+        self.__owner = None # Dueño de la propiedad
         super().__init__(name)
 
     def get_fee(self):
@@ -171,6 +226,9 @@ class Property(Cell):
 
     def get_value(self):
         return self.__value
+
+    def set_fee(self, amount, ):
+        self.__fee = amount
 
     def get_owner(self):
         return self.__owner
--- main.py
+++ main.py
@@ -1,13 +1,6 @@
+import time
+
 from entities import *
-
-
-# Simula el lanzamiento de un dado
-def throw_dice():
-    """
-    Simulates a dice
-    :return: the result of throwing a dice
-    """
-    return np.random.randint(1, 6)
 
 
 # Ordena los jugadores en orden de aquel que saca mayores valores en los dados
@@ -19,10 +12,10 @@ def sort_players(players):
     """
     result = np.zeros((len(players)))
 
-    for i, _ in enumerate(players):
-        result[i] = throw_dice()  # Almacena los resultados de cada lanzamiento de dados
+    for i, player in enumerate(players):
+        result[i] = player.throw_dice()  # Almacena los resultados de cada lanzamiento de dados
 
-    order = np.argsort(result, axis=0).astype(int)  # Da el nuevo ordenamiendo
+    order = np.argsort(result, axis=0).astype(int)[::-1]  # Da el nuevo ordenamiendo
     return [players[i] for i in order]  # Retorna la lista de jugadores en orden de lanzamiento
 
 
@@ -31,28 +24,36 @@ def initialize_game(money=1500):
     board = [
         Cell('GO'),
         Property(name='Mediterranean Avenue', value=40, fee=2),
+        Card(),
         Property(name='Baltic Avenue', value=60, fee=4),
         Property(name='Oriental Avenue', value=100, fee=6),
+        Card(),
         Property(name='Vermont Avenue', value=100, fee=6),
         Property(name='Connecticut Avenue', value=100, fee=8),
         Jail(),
         Property(name='St. Charles place', value=120, fee=10),
+        Card(),
         Property(name='States Avenue', value=140, fee=10),
         Property(name='Virginia Avenue', value=160, fee=12),
         Property(name='St James Place', value=180, fee=14),
+        Card(),
         Property(name='Tennessee Avenue', value=180, fee=14),
         Property(name='New York Avenue', value=200, fee=16),
         Cell('Free Parking'),
         Property(name='Kentucky Avenue', value=220, fee=18),
+        Card(),
         Property(name='Indiana Avenue', value=220, fee=18),
         Property(name='Illinois Avenue', value=240, fee=20),
         Property(name='Atlantic Avenue', value=260, fee=22),
         Property(name='Ventnor Avenue', value=260, fee=22),
+        Card(),
         Property(name='Marvin Gardens', value=280, fee=24),
         Jail(),
         Property(name='Pacific Avenue', value=300, fee=26),
         Property(name='North Carolina Avenue', value=300, fee=26),
+        Card(),
         Property(name='Pennsilvania Avenue', value=320, fee=28),
+        Card(),
         Property(name='Park Place', value=350, fee=35),
         Property(name='Boardwalk', value=400, fee=50)
     ]
@@ -71,16 +72,33 @@ def initialize_game(money=1500):
 
 # Revisar Lógica
 def play(board, players, summary=True):
+    active_players = np.ones((len(players)))
     bankrupt_players = []
+    counter = 1
+    if summary:
+        print(f"Orden de lanzamiento de dados: {[player.name for player in players]}\n")
+
     while len(players) >= 2:  # Mientras haya al menos dos jugadores en juego
-        for i, player in enumerate(players):  # Hace que todos los jugadores activos jueguen
-            if len(players) < 2:  # Apenas gana alguien no se juega más
-                break
-            movement = throw_dice() + throw_dice()  # Lanzamiendo de los dados
+
+        for player in players:  # Hace que todos los jugadores activos jueguen
+            movement = player.throw_dice() + player.throw_dice()  # Lanzamiendo de los dados
             player.turn(movement, board, players)  # Juega el turno de un jugador
-            if player.bankruptcy():  # Si un jugador entra en bancarrota los saca de los jugadores activos y lo mete en los de bancarrota
+
+            if counter % 100 == 0:
+                for prop in board:
+                    if prop.get_name() not in ['GO', 'Card', 'Free Parking', 'Jail']:
+                        prop.set_fee(prop.get_fee() * 2.0)
+
+            if len(players) == 2 and player.bankruptcy():
+                break
+            counter += 1
+        # counter += 1
+
+        for player in players:
+            # Si un jugador entra en bancarrota los saca de los jugadores activos y lo mete en la lista de bancarrota
+            if player.bankruptcy():
                 bankrupt_players.append(player)
-                players.pop(i)
+                players.remove(player)
 
     # Imprime un resumen del juego
     if summary:
@@ -89,25 +107,29 @@ def play(board, players, summary=True):
             f"Resumen del juego:\n{players[0].name} gastó: {players[0].spend * -1} y ganó: {players[0].earned}  jugando {players[0].turn_counter} turnos y {players[0].go_counter} vueltas al tablero")
         for bankrupt_player in reversed(bankrupt_players):
             print(
-                f"{bankrupt_player.name} gastó: {bankrupt_player.spend * -1} y ganó: {bankrupt_player.earned} jugando {bankrupt_player.turn_counter} turnos y {bankrupt_player.go_counter} vueltas al tablero")
+                f"{bankrupt_player.name} gastó: {bankrupt_player.spend * -1} y ganó: {bankrupt_player.earned} quedando con una deuda de {bankrupt_player.debt} jugando {bankrupt_player.turn_counter} turnos y {bankrupt_player.go_counter} vueltas al tablero")
 
     return players, bankrupt_players
 
 
 def main():
-    # Dinero inicial para todos los jugadores
-    money = 1500
-    board, players = initialize_game(money)
 
-    # Ordena el los jugadores e imprime el orden
-    players = sort_players(players)
-    print(f"Orden de lanzamiento de dados: {[player.name for player in players]}\n")
+    trials = 1000
+    for _ in range(trials):
+        # Dinero inicial para todos los jugadores
+        money = 1500
+        board, players = initialize_game(money)
 
-    # Se juega la partida de Monopoly
-    players, bankrupt_players = play(board, players, summary=True)
+        # Ordena el los jugadores e imprime el orden
+        players = sort_players(players)
+
+        # Se juega la partida de Monopoly y se retorna los jugadores con sus estadisticas
+        players, bankrupt_players = play(board, players, summary=False)
 
     return -1
 
 
 if __name__ == '__main__':
+    t = time.time()
     main()
+    print(f"Execution time: {time.time() - t:.5f}[s]")
